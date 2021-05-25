153cb36b9101b23b9e154d832c54295fb7cfd236
Alvin Cousins <alvin.cousins@googlemail.com>
2021-05-25 16:37:03 +0100

    Deployment

--- server.js
+++ server.js
@@ -13,9 +13,12 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("DB connection successful"));
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
